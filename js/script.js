a0932cae05ea4d2e9a5df8f261d486270712ca21
Stephanie Man <Stephanie.Man@aurora-digital.net>
2021-08-13 08:41:32 +0100

    Update to displaying phrases

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ var generateBtn = document.querySelector("#magic-btn");
 // Attach event listener to the magic button
 generateBtn.addEventListener("click", displayMagicPhrase);
 
-// Create array
+// Create questions array
+var questionsArray = [
+  "Do I like to do sports?",
+  "Do I like to make puzzles?",
+  "Do I like pets?",
+  "Do I like pizza",
+  "Do I like to draw or to read?"
+]
+
+// Create phrases array
 var phrasesArray = [
   "Your future is doomed, but hey, you can always try again",
   "Something amazing is coming your way tomorrow",
@@ -13,33 +22,25 @@ var phrasesArray = [
 ];
 
 function displayMagicPhrase() {
-
+  
   var magicPhrase = makeSomeMagic();
   var magicPhraseDisplay = document.querySelector("#magic-phrase");
-//  magicPhraseDisplay.value = "heyaaa";
   magicPhraseDisplay.value = magicPhrase;
   
 }
 
 
 // Get random item from array
-function makeSomeMagic()
-{
-  var q1 = confirm("Do you want to make a snowman?");
-  var q2 = confirm("Are you a happy person?");
-  var q3 = confirm("Do you like rainbows?");
-
-
-  if (q1 == true)
+function makeSomeMagic(){
+  
+  for (let question of questionsArray)
   {
-    quote = "Yesss"
-  }
+    confirm(question);
+  } 
 
-  var magicPhrase = phrasesArray[ 
-    Math.floor(Math.random()*phrasesArray.length)
-  ];
-
+  const magicPhrase = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
   return magicPhrase;
+
 }
 
 

0822473e3a9374ee8b21acd1f16153dbf2703041
ste-123 <stephanieswman@gmail.com>
2021-08-18 23:06:46 +0100

    Updated function to generate phrase.

--- js/script.js
+++ js/script.js
@@ -4,23 +4,6 @@ var generateBtn = document.querySelector("#magic-btn");
 // Attach event listener to the magic button
 generateBtn.addEventListener("click", displayMagicPhrase);
 
-// Create questions array
-var questionsArray = [
-  "Do I like to do sports?",
-  "Do I like to make puzzles?",
-  "Do I like pets?",
-  "Do I like pizza",
-  "Do I like to draw or to read?"
-]
-
-// Create phrases array
-var phrasesArray = [
-  "Your future is doomed, but hey, you can always try again",
-  "Something amazing is coming your way tomorrow",
-  "You will have a long and happy life",
-  "You will be attacked by a fluffy bunny."
-];
-
 function displayMagicPhrase() {
   
   var magicPhrase = makeSomeMagic();
@@ -29,17 +12,22 @@ function displayMagicPhrase() {
   
 }
 
-
-// Get random item from array
+// Get magic phrase
 function makeSomeMagic(){
   
-  for (let question of questionsArray)
-  {
-    confirm(question);
-  } 
+  const likeSports = confirm("Do you like to do sports?");
+  const likePuzzles = confirm("Do you like to make puzzles?");
+  const likePets = confirm("Do you like pets?");
+  const likePizza = confirm("Do you like pizza?");
 
-  const magicPhrase = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
-  return magicPhrase;
+  if (likeSports == true && likePuzzles == true && likePets== true && likePizza == true)
+  {
+     return "Your future as a developer will be full of sunshine and rainbows :)"
+  }
+  else{
+    return "Your future is doomed.  But hey, you can always try again :)";
+  }
+
 
 }
 
